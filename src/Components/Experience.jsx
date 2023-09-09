f3f5790dd261d048f8d76656ab276d7be02919f4
michael <michaelmadlos@hotmail.com>
2023-09-09 11:26:30 +0200

    Add styles to experience section

--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -4,27 +4,31 @@ function Experience() {
 	return (
 		<section className="experience">
 			<h2>Experiencia laboral</h2>
-			<CardExperience
-				position="Nombre de la posición"
-				companyName="Nombre empresa"
-				startDate="2000"
-				endDate="2020"
-				description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem praesentium voluptatibus unde, cumque omnis sed? "
-			/>
-			<CardExperience
-				position="Nombre de la posición"
-				companyName="Nombre empresa"
-				startDate="2000"
-				endDate="2020"
-				description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem praesentium voluptatibus unde, cumque omnis sed? Exercitationem delectus itaque sint beatae ipsam nisi reiciendis? Cupiditate, eos voluptatibus. Officia dolore corrupti sunt!"
-			/>
-			<CardExperience
-				position="Nombre de la posición"
-				companyName="Nombre empresa"
-				startDate="2000"
-				endDate="2020"
-				description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem praesentium voluptatibus unde, cumque omnis sed? Exercitationem delectus itaque sint beatae ipsam nisi reiciendis? "
-			/>
+			<div className="cards-container">
+				<CardExperience
+					position="Nombre de la posición"
+					companyName="Nombre empresa"
+					startDate="2000"
+					endDate="2020"
+					description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem praesentium voluptatibus unde, cumque omnis sed? "
+				/>
+				<div className="separator"></div>
+				<CardExperience
+					position="Nombre de la posición"
+					companyName="Nombre empresa"
+					startDate="2000"
+					endDate="2020"
+					description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem praesentium voluptatibus unde, cumque omnis sed? Exercitationem delectus itaque sint beatae ipsam nisi reiciendis? Cupiditate, eos voluptatibus. Officia dolore corrupti sunt!"
+				/>
+				<div className="separator"></div>
+				<CardExperience
+					position="Nombre de la posición"
+					companyName="Nombre empresa"
+					startDate="2000"
+					endDate="2020"
+					description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem praesentium voluptatibus unde, cumque omnis sed? Exercitationem delectus itaque sint beatae ipsam nisi reiciendis? "
+				/>
+			</div>
 		</section>
 	)
 }
--- src/Components/Units/experience-card.jsx
+++ src/Components/Units/experience-card.jsx
@@ -2,8 +2,12 @@ function CardExperience({ position, companyName, startDate, endDate, description
 	return (
 		<div className="experience-card">
 			<h3>{position}</h3>
-			<h4>{companyName}</h4>
-			<p>{`(${startDate} - ${endDate})`}</p>
+			<i className="fa-solid fa-pencil"></i>
+
+			<div className="company-container">
+				<h4>{companyName}</h4>
+				<p>{`(${startDate} - ${endDate})`}</p>
+			</div>
 			<p>{description}</p>
 		</div>
 	)
