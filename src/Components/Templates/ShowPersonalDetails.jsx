f5f074e7c993f350a6193cf7adc2bcfbb77d7cf1
michael <michaelmadlos@hotmail.com>
2023-09-16 19:04:19 +0200

    Add functionality to upload image as an avatar

--- src/Components/Templates/ShowPersonalDetails.jsx
+++ src/Components/Templates/ShowPersonalDetails.jsx
@@ -1,11 +1,35 @@
+import { useState } from "react"
+
 function ShowPersonalDetails({ personalInfo, onClick }) {
 	const { fullName, location, email, linkedin } = personalInfo
+
+	const [imageProfile, setImageProfile] = useState(undefined)
+
+	function handleOnClickPicture() {
+		const upload = document.getElementById("upload-picture")
+		upload.click()
+		upload.addEventListener("change", () => {
+			const file = URL.createObjectURL(upload.files[0])
+			setImageProfile(file)
+		})
+	}
 
 	return (
 		<section className="personal-details">
 			<div className="personal-details-background"></div>
 			<div className="personal-data">
-				<div className="picture"></div>
+				<div
+					className="picture"
+					onClick={handleOnClickPicture}
+					style={{ backgroundImage: `url(${imageProfile})` }}>
+					<i className="fa-solid fa-pencil"></i>
+					<input
+						type="file"
+						id="upload-picture"
+						className="hidden"
+						accept=".png, .jpg, .jpeg"
+					/>
+				</div>
 				<div className="name">
 					<h1>{fullName ? fullName : "My name"}</h1>
 					<i
